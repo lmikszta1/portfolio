4c711726de94b51da05552b27ebb40966a5a8d73
lmikszta1 <lmikszta@yahoo.com>
2023-07-25 17:22:02 -0400

    changes font color on landing page

--- app/page.jsx
+++ app/page.jsx
@@ -16,8 +16,8 @@ export default function Home() {
             <div className='flex justify-center mt-5 slideInUp delay-300'>
                 <Image src={profilePic} width={225} height={100} alt="LM pic" className='rounded-full border-2 border-slate-300' quality={100}/>
             </div>
-            <h1 className="text-6xl text-slate-300 flex justify-center mt-5 font-roboto slideInUp delay-200 text-center">Luke Mikszta | Full Stack Developer</h1>
-            <main className="flex min-h-max flex-col items-center justify-evenly p-7 text-slate-300">
+            <h1 className="text-6xl text-slate-400 flex justify-center mt-5 font-roboto slideInUp delay-200 text-center">Luke Mikszta | Full Stack Developer</h1>
+            <main className="flex min-h-max flex-col items-center justify-evenly p-7 text-slate-400">
                 <div className='justify-self-start text-center w-1/2 slideInUp delay-150'>
                     <p className='text-2xl font-roboto'>Welcome to my portfolio site!</p>
                     <p className='text-xl font-roboto mt-5'>I&apos;m a full stack developer currently based in Orlando, Florida and I&apos;m looking for work! I&apos;m willing to relocate or work remotely. If you are interested in working with me feel free to connect on LinkedIn and send me a message!</p>
